f5a4d3af053d668df3740d3b3dd535717a4faedb
Dastan <73186335+DastanSmagulov@users.noreply.github.com>
2023-03-16 14:53:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 /////////////////////////////////////////////////
 // Data
 const account1 = {
-  owner: 'Smagulov Dastan',
+  owner: 'Dastan Smagulov',
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
